944a018ed46b85d6b58afd0fb3aab9c5531ba10d
Paresh <pareshnathnandi1234@gmail.com>
2024-03-19 22:15:06 +0530

    added Chat Box fontend

---
+++ Coching center/index.js
@@ -0,0 +1,14 @@
+//will use it later ,Currently this is not working 
+
+// const passwordInput = document.getElementById('passwordInput');
+// const showPasswordBtn = document.getElementById('showPasswordBtn');
+
+// showPasswordBtn.addEventListener('click', function() {
+//   if (passwordInput.type === 'password') {
+//     passwordInput.type = 'text';
+//     showPasswordBtn.textContent = 'Hide Password';
+//   } else {
+//     passwordInput.type = 'password';
+//     showPasswordBtn.textContent = 'Show Password';
+//   }
+// });
